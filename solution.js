a16516ff4db97dcd6b46f1d5ec1e342d7e657b5d
github username <github email address>
2019-05-08 14:28:39 -0400

    done.

--- solution.js
+++ solution.js
@@ -1,1 +1,28 @@
+const data = [
+    [409, 194, 207, 470, 178],
+    [454, 235, 333, 511, 103],
+    [474, 293, 525, 372, 408],
+    [428, 4321, 2786, 6683, 3921],
+    [265, 262, 6206, 2207, 5712]
+  ]
 
+function checkSum(data) {
+    let sum = 0
+    data.forEach( function (row) {
+        let minNum = row[0];
+        let maxNum = row[0];
+        let rowDiff;
+        row.forEach (function (num) {
+            if (maxNum < num) {
+                maxNum = num 
+            } else if (minNum > num) {
+                minNum = num
+            };
+        });
+        rowDiff = maxNum - minNum;
+        sum = sum + rowDiff;
+    })
+    return sum;
+}
+
+checkSum(data);
